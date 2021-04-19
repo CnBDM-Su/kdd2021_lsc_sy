775dabf67e505745a54e6508532dd604f5609a8d
suy <su.yu@intellif.com>
2021-04-19 17:47:35 +0800

    'update'

--- examples/lsc/mag240m/analysis.py
+++ examples/lsc/mag240m/analysis.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
     train_idx = dataset.get_idx_split('train')
     # valid_idx = dataset.get_idx_split('valid')
     # test_idx = dataset.get_idx_split('test')
-    valid_idx = dataset.get_idx_split('valid')
+    valid_idx = dataset.get_idx_split('test')
 
     t = time.perf_counter()
     print('Reading training node features...', end=' ', flush=True)
