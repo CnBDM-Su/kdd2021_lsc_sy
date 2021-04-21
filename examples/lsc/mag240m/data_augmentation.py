9fcf8ee9d9e28a40c95927a285ff89058d999043
suy <su.yu@intellif.com>
2021-04-21 19:20:15 +0800

    'update'

--- examples/lsc/mag240m/data_augmentation.py
+++ examples/lsc/mag240m/data_augmentation.py
@@ -129,7 +129,7 @@ if __name__ == '__main__':
         device = 'cpu'
         print('cpu')
 
-    sup = np.load(ROOT+'sup.npy')
+    sup = np.load(ROOT+'/sup.npy')
     dataset = MAG240MDataset(ROOT)
     evaluator = MAG240MEvaluator()
 
