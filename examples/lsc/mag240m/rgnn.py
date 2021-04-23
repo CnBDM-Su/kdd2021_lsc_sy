3797c94af8b90283cae809b10f5ca35c1ebbd0d3
suy <su.yu@intellif.com>
2021-04-23 18:06:22 +0800

    'update'

--- examples/lsc/mag240m/rgnn.py
+++ examples/lsc/mag240m/rgnn.py
@@ -282,7 +282,7 @@ class MAG240M(LightningDataModule):
         # x = torch.from_numpy(np.array(x)).to(torch.float)
         print(n_id.shape)
         x = torch.from_numpy(self.x[n_id.numpy()]).to(torch.float)
-        print(sys.getsizeof(x))
+        print(sys.getsizeof(x.storage()))
         y = self.y[n_id[:batch_size]].to(torch.long)
         print(f'Done sampling! [{time.perf_counter() - t:.2f}s]')
         return Batch(x=x, y=y, adjs_t=[adj_t for adj_t, _, _ in adjs])
