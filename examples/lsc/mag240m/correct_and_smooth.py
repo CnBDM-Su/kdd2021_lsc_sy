1fdfde09e34a71c95b4bcfd90821cb0840e50480
suy <su.yu@intellif.com>
2021-04-30 17:05:14 +0800

    'update'

--- examples/lsc/mag240m/correct_and_smooth.py
+++ examples/lsc/mag240m/correct_and_smooth.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
     t = time.perf_counter()
     y_pred = torch.from_numpy(np.load('results/cs/pred.npy'))
     print(f'Done! [{time.perf_counter() - t:.2f}s]')
+    print(abs((float(y_pred.sum()) / y_pred.size(0)) - 1.0))
 
     t = time.perf_counter()
     print('Reading adjacency matrix...', end=' ', flush=True)
