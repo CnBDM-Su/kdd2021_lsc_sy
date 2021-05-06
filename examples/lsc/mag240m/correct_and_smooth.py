5677dde57e0c675621f15da9c0b3392b682dfbe3
suy <su.yu@intellif.com>
2021-05-06 08:38:59 +0800

    'update'

--- examples/lsc/mag240m/correct_and_smooth.py
+++ examples/lsc/mag240m/correct_and_smooth.py
@@ -7,10 +7,87 @@ import argparse
 import torch
 import numpy as np
 from torch_sparse import SparseTensor
-from torch_geometric.nn import CorrectAndSmooth
+# from torch_geometric.nn import CorrectAndSmooth
 from torch_geometric.nn.conv.gcn_conv import gcn_norm
 from ogb.lsc import MAG240MDataset, MAG240MEvaluator
 from root import ROOT
+from torch_geometric.typing import Adj, OptTensor
+
+from torch import Tensor
+import torch.nn.functional as F
+
+from torch_geometric.nn.models import LabelPropagation
+
+
+class CorrectAndSmooth(torch.nn.Module):
+
+    def __init__(self, num_correction_layers: int, correction_alpha: float,
+                 num_smoothing_layers: int, smoothing_alpha: float,
+                 autoscale: bool = True, scale: float = 1.0):
+        super(CorrectAndSmooth, self).__init__()
+        self.autoscale = autoscale
+        self.scale = scale
+
+        self.prop1 = LabelPropagation(num_correction_layers, correction_alpha)
+        self.prop2 = LabelPropagation(num_smoothing_layers, smoothing_alpha)
+
+    def correct(self, y_soft: Tensor, y_true: Tensor, mask: Tensor,
+                edge_index: Adj, edge_weight: OptTensor = None) -> Tensor:
+
+        assert abs((float(y_soft.sum()) / y_soft.size(0)) - 1.0) < 1e-2
+
+        numel = int(mask.sum()) if mask.dtype == torch.bool else mask.size(0)
+        assert y_true.size(0) == numel
+
+        if y_true.dtype == torch.long:
+            y_true = F.one_hot(y_true.view(-1), y_soft.size(-1))
+            y_true = y_true.to(y_soft.dtype)
+
+        error = torch.zeros_like(y_soft)
+        error[mask] = y_true - y_soft[mask]
+
+        if self.autoscale:
+            smoothed_error = self.prop1(error, edge_index,
+                                        edge_weight=edge_weight,
+                                        post_step=lambda x: x.clamp_(-1., 1.))
+
+            sigma = error[mask].abs().sum() / numel
+            scale = sigma / smoothed_error.abs().sum(dim=1, keepdim=True)
+            scale[scale.isinf() | (scale > 1000)] = 1.0
+            return y_soft + scale * smoothed_error
+        else:
+
+            def fix_input(x):
+                x[mask] = error[mask]
+                return x
+
+            smoothed_error = self.prop1(error, edge_index,
+                                        edge_weight=edge_weight,
+                                        post_step=fix_input)
+            return y_soft + self.scale * smoothed_error
+
+    def smooth(self, y_soft: Tensor, y_true: Tensor, mask: Tensor,
+               edge_index: Adj, edge_weight: OptTensor = None) -> Tensor:
+
+        numel = int(mask.sum()) if mask.dtype == torch.bool else mask.size(0)
+        assert y_true.size(0) == numel
+
+        if y_true.dtype == torch.long:
+            y_true = F.one_hot(y_true.view(-1), y_soft.size(-1))
+            y_true = y_true.to(y_soft.dtype)
+
+        y_soft[mask] = y_true
+
+        return self.prop2(y_soft, edge_index, edge_weight=edge_weight)
+
+    def __repr__(self):
+        L1, alpha1 = self.prop1.num_layers, self.prop1.alpha
+        L2, alpha2 = self.prop2.num_layers, self.prop2.alpha
+        return (f'{self.__class__.__name__}(\n'
+                f'    correct: num_layers={L1}, alpha={alpha1}\n'
+                f'    smooth:  num_layers={L2}, alpha={alpha2}\n'
+                f'    autoscale={self.autoscale}, scale={self.scale}\n'
+                ')')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -58,13 +135,42 @@ if __name__ == '__main__':
     y_train = torch.from_numpy(dataset.paper_label[train_idx]).to(torch.long)
     y_valid = torch.from_numpy(dataset.paper_label[valid_idx]).to(torch.long)
 
-    model = CorrectAndSmooth(args.num_correction_layers, args.correction_alpha,
-                             args.num_smoothing_layers, args.smoothing_alpha,
-                             autoscale=True)
+    # model = CorrectAndSmooth(args.num_correction_layers, args.correction_alpha,
+    #                          args.num_smoothing_layers, args.smoothing_alpha,
+    #                          autoscale=True)
 
     t = time.perf_counter()
     print('Correcting predictions...', end=' ', flush=True)
-    y_pred = model.correct(y_pred, y_train, train_idx, adj_t)
+    assert abs((float(y_pred.sum()) / y_pred.size(0)) - 1.0) < 1e-2
+
+    numel = int(train_idx.sum()) if train_idx.dtype == torch.bool else train_idx.size(0)
+    assert y_train.size(0) == numel
+
+    if y_train.dtype == torch.long:
+        y_true = F.one_hot(y_train.view(-1), y_pred.size(-1))
+        y_true = y_true.to(y_pred.dtype)
+
+    error = torch.zeros_like(y_pred)
+    error[train_idx] = y_train - y_pred[train_idx]
+    prop1 = LabelPropagation(args.num_correction_layers, args.correction_alpha)
+    del y_pred
+    del y_train
+
+    smoothed_error = prop1(error, adj_t,
+                                edge_weight=edge_weight,
+                                post_step=lambda x: x.clamp_(-1., 1.))
+
+    sigma = error[train_idx].abs().sum() / numel
+    scale = sigma / smoothed_error.abs().sum(dim=1, keepdim=True)
+    scale[scale.isinf() | (scale > 1000)] = 1.0
+
+    print('Reading MLP soft prediction...', end=' ', flush=True)
+    t = time.perf_counter()
+    y_pred = torch.from_numpy(np.load('results/cs/pred.npy'))
+    print(f'Done! [{time.perf_counter() - t:.2f}s]')
+
+    y_pred = y_pred + scale * smoothed_error
+    # y_pred = model.correct(y_pred, y_train, train_idx, adj_t)
     print(f'Done! [{time.perf_counter() - t:.2f}s]')
 
     t = time.perf_counter()
