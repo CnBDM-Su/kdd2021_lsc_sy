a4ccdf24f8b3dc3926cf6dff71a36929dd5e1d5e
suy <su.yu@intellif.com>
2021-05-17 14:42:08 +0800

    update

--- examples/lsc/mag240m/ignore_unraleted_node.py
+++ examples/lsc/mag240m/ignore_unraleted_node.py
@@ -15,64 +15,7 @@ from ogb.lsc import MAG240MDataset, MAG240MEvaluator
 from root import ROOT
 
 
-class MLP(torch.nn.Module):
-    def __init__(self, in_channels: int, hidden_channels: int,
-                 out_channels: int, num_layers: int, dropout: float = 0.0,
-                 batch_norm: bool = True, relu_last: bool = False):
-        super(MLP, self).__init__()
 
-        self.lins = ModuleList()
-        self.lins.append(Linear(in_channels, hidden_channels))
-        for _ in range(num_layers - 2):
-            self.lins.append(Linear(hidden_channels, hidden_channels))
-        self.lins.append(Linear(hidden_channels, out_channels))
-
-        self.batch_norms = ModuleList()
-        for _ in range(num_layers - 1):
-            norm = BatchNorm1d(hidden_channels) if batch_norm else Identity()
-            self.batch_norms.append(norm)
-
-        self.dropout = dropout
-        self.relu_last = relu_last
-
-    def reset_parameters(self):
-        for lin in self.lins:
-            lin.reset_parameters()
-        for batch_norm in self.batch_norms:
-            batch_norm.reset_parameters()
-
-    def forward(self, x):
-        for lin, batch_norm in zip(self.lins[:-1], self.batch_norms):
-            x = lin(x)
-            if self.relu_last:
-                x = batch_norm(x).relu_()
-            else:
-                x = batch_norm(x.relu_())
-            x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.lins[-1](x)
-        return x
-
-
-def train(model, x_train, y_train, batch_size, optimizer):
-    model.train()
-
-    total_loss = 0
-    for idx in DataLoader(range(y_train.size(0)), batch_size, shuffle=True):
-        optimizer.zero_grad()
-        loss = F.cross_entropy(model(x_train[idx]), y_train[idx])
-        loss.backward()
-        optimizer.step()
-
-        total_loss += float(loss) * idx.numel()
-
-    return total_loss / y_train.size(0)
-
-
-@torch.no_grad()
-def test(model, x_eval, y_eval, evaluator):
-    model.eval()
-    y_pred = model(x_eval).argmax(dim=-1)
-    return evaluator.eval({'y_true': y_eval, 'y_pred': y_pred})['acc']
 
 
 if __name__ == '__main__':
@@ -87,7 +30,6 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=380000)
     parser.add_argument('--epochs', type=int, default=1000)
     parser.add_argument('--evaluate', type=bool, default=False)
-    parser.add_argument('--p_batch_size', type=int, default=40000)
     args = parser.parse_args()
     print(args)
 
@@ -101,50 +43,15 @@ if __name__ == '__main__':
     valid_idx = dataset.get_idx_split('valid')
     test_idx = dataset.get_idx_split('test')
 
-    path = f'{dataset.dir}/sorted_author_paper_edge.npy'
-    if not osp.exists(path):
-        print('Generating sorted author paper edges...')
-        t = time.perf_counter()
-        ap_edge = dataset.edge_index('author', 'writes', 'paper')
-        ap_edge = ap_edge[:, ap_edge[1, :].argsort()]
-        np.save(path, ap_edge)
-        print(f'Done! [{time.perf_counter() - t:.2f}s]')
+    meaningful_idx = np.concatenate([train_idx, valid_idx, test_idx], 0)
+    meaningful_idx = np.sort(meaningful_idx)
 
-    path = f'{dataset.dir}/paper_relation_feat.npy'
-    if not osp.exists(path):
-        print('Generating paper relation features...')
-        t = time.perf_counter()
-        N = dataset.num_papers + dataset.num_authors + dataset.num_institutions
-        x = np.memmap(f'{dataset.dir}/full_feat.npy', dtype=np.float16,
-                           mode='r', shape=(N, 768))
-        y = np.memmap(path, dtype=np.float16, mode='w+',
-                      shape=(dataset.num_papers, 1536))
-        ap_edge = np.load(f'{dataset.dir}/sorted_author_paper_edge.npy')
-        bias = 0
-        p_batch_size = args.p_batch_size
-        for p_batch in tqdm(range(dataset.num_papers // p_batch_size)):
-            fea_ = []
-            end = min((p_batch + 1) * p_batch_size, dataset.num_papers)
-            for i in range(p_batch * p_batch_size, end):
-                sign = 0
-                fea = []
-                for j in range(bias, len(ap_edge[0])):
-                    if ap_edge[1, j] == i:
-                        fea.append(ap_edge[0, j])
-                        sign = 1
-                    if len(fea) > 20:
-                        break
-                    if (sign == 1) and (ap_edge[1, j] != i):
-                        break
-                bias = j
-                fea = x[fea]
-                fea_.append(np.mean(fea, 0))
-            fea_ = np.array(fea_)
-            y[p_batch * p_batch_size:end] = np.concatenate([x[p_batch * p_batch_size:end], fea_], 1)
-        print(f'Done! [{time.perf_counter() - t:.2f}s]')
-    else:
-        x_fr = np.memmap(path, dtype=np.float16, mode='r',
-                      shape=(dataset.num_papers, 1536))
+    path = f'{dataset.dir}/paper_to_paper_symmetric.pt'
+    if osp.exists(path):
+        adj_t = torch.load(path)
+
+    for i in meaningful_idx:
+
 
     if args.evaluate==False:
         t = time.perf_counter()
