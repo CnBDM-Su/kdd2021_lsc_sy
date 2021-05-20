8d97ad76c6359274de342ac92f453b96ff7b8420
suy <su.yu@intellif.com>
2021-05-20 15:35:39 +0800

    update

--- examples/lsc/mag240m/correct_and_smooth.py
+++ examples/lsc/mag240m/correct_and_smooth.py
@@ -12,6 +12,7 @@ from torch_geometric.nn.conv.gcn_conv import gcn_norm
 from ogb.lsc import MAG240MDataset
 from root import ROOT
 from ogb.utils.url import makedirs
+import sys
 sys.path.append('/var/ogb/ogb/lsc')
 from mag240m_mini_graph import MAG240MMINIDataset
 class MAG240MEvaluator:
@@ -64,7 +65,6 @@ if __name__ == '__main__':
 
     if args.mini_graph:
         dataset = MAG240MMINIDataset(ROOT)
-
 
     else:
         dataset = MAG240MDataset(ROOT)
