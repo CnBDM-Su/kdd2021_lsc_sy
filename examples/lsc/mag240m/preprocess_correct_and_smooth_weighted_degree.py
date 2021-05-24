e751af27c271206a7b1742ad4d3f8746e353a860
suy <su.yu@intellif.com>
2021-05-24 17:42:47 +0800

    update

--- examples/lsc/mag240m/preprocess_correct_and_smooth_weighted_degree.py
+++ examples/lsc/mag240m/preprocess_correct_and_smooth_weighted_degree.py
@@ -135,10 +135,10 @@ if __name__ == '__main__':
         row, col, val = torch.from_numpy(weighted_edge)
         degree = np.load(f'{dataset.dir}/author_degree.npy')
         degree = torch.from_numpy(np.diag(degree.astype(float)**(-1)))
-        degree = SparseTensor(
-            row=torch.from_numpy(np.arange(dataset.num_authors)).long(), col=torch.from_numpy(np.arange(dataset.num_authors)).long(), value=degree.float(),
-            sparse_sizes=(dataset.num_authors, dataset.num_authors),
-            is_sorted=True)
+        # degree = SparseTensor(
+        #     row=torch.from_numpy(np.arange(dataset.num_authors)).long(), col=torch.from_numpy(np.arange(dataset.num_authors)).long(), value=degree.float(),
+        #     sparse_sizes=(dataset.num_authors, dataset.num_authors),
+        #     is_sorted=True)
         adj_t = SparseTensor(
             row=col.long(), col=row.long(), value=val.float(),
             sparse_sizes=(dataset.num_papers, dataset.num_authors),
