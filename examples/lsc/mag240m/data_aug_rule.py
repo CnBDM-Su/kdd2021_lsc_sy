246e4eab579add92687d023af9019879d3da2545
suy <su.yu@intellif.com>
2021-05-25 19:34:54 +0800

    update

--- examples/lsc/mag240m/data_aug_rule.py
+++ examples/lsc/mag240m/data_aug_rule.py
@@ -12,9 +12,9 @@ from mag240m_mini_graph import MAG240MMINIDataset
 dataset = MAG240MMINIDataset(ROOT)
 
 train_idx = dataset.get_idx_split('train')
-te_id = np.random.choice(train_idx.shape[0], size=(int(np.round(train_idx.shape[0]*0.2)),), replace=False)
-te_idx = np.sort(train_idx[te_id])
-train_idx = np.sort(np.array(list(set(train_idx) - set(te_idx))))
+# te_id = np.random.choice(train_idx.shape[0], size=(int(np.round(train_idx.shape[0]*0.2)),), replace=False)
+# te_idx = np.sort(train_idx[te_id])
+# train_idx = np.sort(np.array(list(set(train_idx) - set(te_idx))))
 valid_idx = dataset.get_idx_split('valid')
 test_idx = dataset.get_idx_split('test')
 idx = np.concatenate([train_idx,valid_idx,test_idx],0)
@@ -44,38 +44,43 @@ for i in a_l.keys():
 print(len(reliable_author.keys()))
 ap_edge = dataset.edge_index('author', 'writes', 'paper')
 #______________test___________________
-new_label = deepcopy(paper_label)
-relate = []
-true = []
-# true = new_label[te_idx]
-bias = 0
-for i in tqdm(range(len(reliable_author.keys()))):
-    i = list(reliable_author.keys())[i]
-    l = reliable_author[i]
-    for j in range(bias,ap_edge.shape[1]):
-        if i==ap_edge[0,j]:
-            if ap_edge[1, j] in te_idx:
-                true.append(new_label[ap_edge[1,j]])
-                relate.append(ap_edge[1, j])
-                new_label[ap_edge[1,j]] = l
-        elif i<ap_edge[0,j]:
-            bias = j
-            break
-
-pred = new_label[relate]
-print(accuracy_score(true,pred))
-
-#______________predict________________
 # new_label = deepcopy(paper_label)
+# relate = []
+# true = []
+# # true = new_label[te_idx]
 # bias = 0
 # for i in tqdm(range(len(reliable_author.keys()))):
-#     i = reliable_author.keys()[i]
+#     i = list(reliable_author.keys())[i]
 #     l = reliable_author[i]
 #     for j in range(bias,ap_edge.shape[1]):
 #         if i==ap_edge[0,j]:
-#             if ap_edge[1,j] not in idx:
+#             if ap_edge[1, j] in te_idx:
+#                 true.append(new_label[ap_edge[1,j]])
+#                 relate.append(ap_edge[1, j])
 #                 new_label[ap_edge[1,j]] = l
 #         elif i<ap_edge[0,j]:
 #             bias = j
 #             break
-# np.save(f'{dataset.dir}/new_paper_label.npy',new_label)
+#
+# pred = new_label[relate]
+# print(accuracy_score(true,pred))
+
+#______________predict________________
+new_label = deepcopy(paper_label)
+new_tr = []
+bias = 0
+for i in tqdm(range(len(reliable_author.keys()))):
+    i = reliable_author.keys()[i]
+    l = reliable_author[i]
+    for j in range(bias,ap_edge.shape[1]):
+        if i==ap_edge[0,j]:
+            if ap_edge[1,j] not in idx:
+                new_tr.append(ap_edge[1,j])
+                new_label[ap_edge[1,j]] = l
+        elif i<ap_edge[0,j]:
+            bias = j
+            break
+new_tr = np.sort(train_idx.tolist() + new_tr)
+np.save(f'{dataset.dir}/new_train_idx.npy',new_tr)
+np.save(f'{dataset.dir}/new_paper_label.npy',new_label)
+
