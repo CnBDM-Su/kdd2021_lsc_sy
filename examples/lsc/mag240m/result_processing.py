82a39fbb924efde6fa1996dd670e77b80d057c51
suy <su.yu@intellif.com>
2021-05-27 11:16:15 +0800

    update

--- examples/lsc/mag240m/result_processing.py
+++ examples/lsc/mag240m/result_processing.py
@@ -163,10 +163,14 @@ if __name__ == '__main__':
     # for i in correct_index:
     #     y_pred_[i] = y_correct[i]
     y_pred_ = deepcopy(y_pred)
+    c=0
     for i in range(valid_idx.shape[0]):
         ind = valid_idx[i]
+        if y_correct[i].sum()!=0:
+            c+=1
         y_pred_[ind] = (y_pred[ind] + y_correct[i])/2
 
+    print(c)
     train_acc = evaluator.eval({
         'y_true': y_train,
         'y_pred': y_pred_.argmax(dim=-1)[train_idx]
