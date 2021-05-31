7e8b897ada0e1fbe8f7cb52428a637aebccb1672
suy <su.yu@intellif.com>
2021-05-31 14:07:39 +0800

    update

--- examples/lsc/mag240m/data_aug_rule.py
+++ examples/lsc/mag240m/data_aug_rule.py
@@ -32,7 +32,7 @@ a_l = np.zeros(shape=(dataset.num_authors,dataset.num_classes))
 for i in tqdm(range(train_idx.shape[0])):
     i = train_idx[i]
     for j in range(bias,ap_edge.shape[1]):
-        if (i==ap_edge[1,j]) and (year[int(ap_edge[0,j])]>=2010):
+        if i==ap_edge[1,j]:
             a_l[int(ap_edge[0,j]),int(paper_label[ap_edge[1,j]])] += 1
         elif i<ap_edge[1,j]:
             bias = j
