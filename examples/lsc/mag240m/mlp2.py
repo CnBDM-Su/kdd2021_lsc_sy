a7ef0b9a708cf7f79e907ec9dacac016d506a080
suy <su.yu@intellif.com>
2021-06-05 09:48:10 +0800

    update

--- examples/lsc/mag240m/mlp2.py
+++ examples/lsc/mag240m/mlp2.py
@@ -210,7 +210,7 @@ if __name__ == '__main__':
             model = torch.nn.DataParallel(model, device_ids=gpus)
         model.load_state_dict(torch.load('results/mlp/model.pkl'))
 #___________________predict______________________________
-        feat = np.load(f'{dataset.dir}/paper_relation_weighted_feat.npy')
+        feat = np.load(f'{dataset.dir}/paper_relation_weighted_i_feat.npy')
         w = torch.t(model.state_dict()['module.lins.0.weight'])
         bias = model.state_dict()['module.lins.0.bias']
         batch_size = 200000
