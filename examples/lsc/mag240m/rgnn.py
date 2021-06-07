a424bc3fc5f0db0523d8ce6e5ad308818c83488c
suy <su.yu@intellif.com>
2021-06-07 15:02:50 +0800

    update

--- examples/lsc/mag240m/rgnn.py
+++ examples/lsc/mag240m/rgnn.py
@@ -586,8 +586,8 @@ if __name__ == '__main__':
 
         else:
             if args.cs:
-                # loader = datamodule.all_dataloader()
-                loader = datamodule.val_dataloader()
+                loader = datamodule.all_dataloader()
+                # loader = datamodule.val_dataloader()
 
                 model.eval()
                 y_preds = []
