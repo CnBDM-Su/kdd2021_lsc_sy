bc21586b46853b048e32c6a2bf4cdeed9b5e0535
suy <su.yu@intellif.com>
2021-06-08 08:58:40 +0800

    update

--- examples/lsc/mag240m/correct_and_smooth.py
+++ examples/lsc/mag240m/correct_and_smooth.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
 
     print('Reading MLP soft prediction...', end=' ', flush=True)
     t = time.perf_counter()
-    y_pred = torch.from_numpy(np.load(save_path+'/256rgat_cs_val0.5.npz.npz')['y_pred'])
+    y_pred = torch.from_numpy(np.load(save_path+'/256rgat_cs_val0.5.npz')['y_pred'])
     # y_pred = torch.from_numpy(np.load(save_path+'/y_pred_mag240m.npz')['y_pred'])
     # y_pred = torch.from_numpy(np.load(save_path+'/pred.npy'))
     # print(y_pred.shape)
